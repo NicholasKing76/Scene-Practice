5fa2ced749c00a7158594475734b85dd5c521ef8
NicholasKing76 <54336920+NicholasKing76@users.noreply.github.com>
2019-09-08 17:50:55 -0500

    Initial commit

---
+++ Assets/Scenes/InputScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InputScript : MonoBehaviour
+{
+    public InputField PlaceName;
+    public Text InputName;
+
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void DisplayName()
+    {
+        InputName.text = PlaceName.text;
+    }
+}
